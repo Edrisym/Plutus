bd822759644623a054444bb0872bae857d2e8850
Eddie <edrismoavenghafouri@gmail.com>
2025-05-09 18:58:50 +0330

    added Transaction Configurations

--- Plutus.Persistence/Configurations/TransactionConfigurations.cs
+++ Plutus.Persistence/Configurations/TransactionConfigurations.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Plutus.Domain.Common;
 using Plutus.Domain.Entities;
 
 namespace Plutus.Persistence.Configurations;
@@ -8,7 +10,30 @@ public class TransactionConfigurations : IEntityTypeConfiguration<Transaction>
 {
     public void Configure(EntityTypeBuilder<Transaction> builder)
     {
-        //TODO
-        throw new NotImplementedException();
+        builder.ToTable(StaticData.TableName.Transactions);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .HasColumnType(StaticData.WalletDbContextDefaultColumnType.Nvarchar36);
+
+        builder.Property(x => x.From)
+            .HasMaxLength(36)
+            .IsRequired();
+
+        builder.Property(x => x.To)
+            .HasMaxLength(36)
+            .IsRequired();
+
+        builder.Property(x => x.Amount)
+            .HasColumnType(StaticData.WalletDbContextDefaultColumnType.Decimal)
+            .IsRequired();
+
+        builder.Property(p => p.State)
+            .HasConversion(new EnumToStringConverter<TransactionState>())
+            .HasColumnName("TransactionState")
+            .HasMaxLength(64)
+            .IsRequired();
     }
 }
