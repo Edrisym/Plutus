5c327ea545f5e30c7328447a5a361890865288b6
Eddie <edrismoavenghafouri@gmail.com>
2025-05-09 18:24:59 +0330

    Added scalar api documentation

--- Plutus.Api/Program.cs
+++ Plutus.Api/Program.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Plutus.Persistence;
+using Scalar.AspNetCore;
 
 namespace Plutus.Api;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var services = builder.Services;
@@ -17,7 +18,14 @@ public static class Program
 
         var app = builder.Build();
 
+        app.MapScalarApiReference(opt =>
+        {
+            opt.Title = "Scalar Example";
+            opt.Theme = ScalarTheme.Mars;
+            opt.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+        });
+
         app.MapControllers();
-        app.Run();
+        await app.RunAsync();
     }
 }
